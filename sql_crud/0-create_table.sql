e729c874e693e8219b0f07bd3109ee2776da8123
aloladze88-afk <aloladze88@gmail.com>
2026-06-02 09:51:16 +0200

    fix 2 create table sql

--- sql_crud/0-create_table.sql
+++ sql_crud/0-create_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE books (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     author TEXT NOT NULL,
-    genre TEXT,
+    genre TEXT NOT NULL,
     price REAL NOT NULL,
     stock INTEGER NOT NULL,
     published_year INTEGER NOT NULL
