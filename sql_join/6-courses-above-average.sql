0d5bdacd24563c6ca67da057cb26b257663abdad
aloladze88-afk <aloladze88@gmail.com>
2026-06-04 18:57:18 +0200

    fix 6

--- sql_join/6-courses-above-average.sql
+++ sql_join/6-courses-above-average.sql
@@ -1,16 +1,14 @@
-SELECT c.title AS course_title
+SELECT c.title
 FROM courses AS c
-LEFT JOIN enrollments AS e
+JOIN enrollments AS e
   ON c.id = e.course_id
 GROUP BY c.id, c.title
 HAVING COUNT(e.student_id) > (
     SELECT AVG(enrollment_count)
     FROM (
-        SELECT COUNT(e2.student_id) AS enrollment_count
-        FROM courses AS c2
-        LEFT JOIN enrollments AS e2
-          ON c2.id = e2.course_id
-        GROUP BY c2.id
-    )
+        SELECT COUNT(*) AS enrollment_count
+        FROM enrollments
+        GROUP BY course_id
+    ) AS course_counts
 )
-ORDER BY course_title ASC;
+ORDER BY c.title ASC;
