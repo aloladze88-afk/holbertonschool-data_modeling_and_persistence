08aa0e447d09a5e55372ed3124abacd2c686c148
aloladze88-afk <aloladze88@gmail.com>
2026-06-04 18:58:00 +0200

    7

---
+++ sql_join/7-course-enrollment-count.sql
@@ -0,0 +1,8 @@
+SELECT
+    c.title AS course_title,
+    COUNT(e.student_id) AS enrollment_count
+FROM courses AS c
+LEFT JOIN enrollments AS e
+    ON c.id = e.course_id
+GROUP BY c.id, c.title
+ORDER BY enrollment_count DESC, course_title ASC;
